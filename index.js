b3578ab0a1872b97cb19f1e0fb343e8f928e912f
jxckles <164807974+jxckles@users.noreply.github.com>
2025-01-18 22:16:13 +0800

    Update index.js

--- index.js
+++ index.js
@@ -66,7 +66,7 @@ mongoose.connect(
 
 
 app.get("/", (req, res) => {
-  res.setHeader("Access-Control-Allow-Credentials", "true);
+  res.setHeader("Access-Control-Allow-Credentials", "true");
   res.send("API is running...");
 });
 
